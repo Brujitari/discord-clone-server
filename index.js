47fcca98fc1a15057e01a88456a38a7507a925dc
Nat <98291454+Brujitari@users.noreply.github.com>
2022-09-06 21:05:55 +0200

    feat: errors (#11)

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const options = require("./configs/cors");
-//const db = require("./configs/db");
+const errors = require("./errors/commons");
+const db = require("./configs/db");
 
 const app = express();
 
@@ -11,8 +12,16 @@ app.use(cors(options));
 app.use(express.json());
 app.use(cookieParser());
 
+app.use((_, __, next) => {
+  next(errors[404]);
+});
 
-
+app.use(({ statusCode, error }, _, res, __) => {
+  res.status(statusCode).json({
+    success: false,
+    message: error.message,
+  });
+});
 
 app.listen(process.env.PORT, () =>
   console.info(`😋🌈 te escucho en el ${process.env.PORT} bb 🦋`)
