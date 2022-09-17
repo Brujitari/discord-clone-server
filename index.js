392a01940d013c545710cea989a777a22bb09e49
Brujitari <natimontesg2@mail.com>
2022-09-18 01:24:39 +0200

    test: socket server routes don't work

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const { PORT } = require("./environments");
 const app = require("./app");
+const http = require('http')
+const server = http.createServer(app)
+const socketio = require('socket.io')
+const io = socketio(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+})
 
+server.listen(process.env.PORT || 3001, () => {
+  console.info(`😋 te escucho en el ${PORT} bb 🌈 entorno: ${process.env.NODE_ENV} 🦋`)
+})
 
-const main = async () => {
-  (await app()).listen(PORT, () =>
-    console.info(
-      `😋 te escucho en el ${PORT} bb 🌈 entorno: ${process.env.NODE_ENV} 🦋`
-    )
-  );
-};
+// const main = async () => {
+//   (await app()).listen(PORT, () =>
+//     console.info(
+//       `😋 te escucho en el ${PORT} bb 🌈 entorno: ${process.env.NODE_ENV} 🦋`
+//     )
+//   );
+// };
 
-main();
+// main();
