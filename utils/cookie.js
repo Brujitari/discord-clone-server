fcfd0b44ae86aa6ec13dfeadf18bbca4abd7b8b4
Brujitari <natimontesg2@mail.com>
2022-09-27 21:02:11 +0200

    fix: secure prod true

--- utils/cookie.js
+++ utils/cookie.js
@@ -1,7 +1,7 @@
 const create = (res, token, extTime = 300000) => {
   res.cookie("access_token", token, {
     expires: new Date(Date.now() + extTime),
-    secure: false,
+    secure: process.env.NODE_ENV === 'production'? true : false,
     httpOnly: true,
   });
 };
